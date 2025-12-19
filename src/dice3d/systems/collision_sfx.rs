8b0e0ad39f2bb182e1ab52b6a3656f453f5e2230
edgarhsanchez <esanchez@m2iab.com>
2025-12-19 01:18:36 -0600

    Debounce collision SFX

--- src/dice3d/systems/collision_sfx.rs
+++ src/dice3d/systems/collision_sfx.rs
@@ -3,6 +3,7 @@
 use bevy::audio::{AudioPlayer, AudioSource, PlaybackSettings};
 use bevy::prelude::*;
 use bevy_rapier3d::prelude::*;
+use std::collections::HashMap;
 
 use crate::dice3d::embedded_assets::{
     DICE_GLASS_CUP_SFX_PATH, DICE_WOODEN_BOX_SFX_PATH,
@@ -15,21 +16,40 @@ pub struct DiceCollisionSfx {
     pub box_: Handle<AudioSource>,
 }
 
+/// Debounce state to avoid spamming collision SFX when Rapier reports multiple contact events
+/// for the same die in quick succession.
+#[derive(Resource, Default)]
+pub struct DiceCollisionSfxDebounce {
+    /// Last playback time (seconds since startup) per die entity.
+    pub last_played_s: HashMap<Entity, f32>,
+    /// Minimum interval between plays per die.
+    pub min_interval_s: f32,
+}
+
 pub fn init_collision_sounds(mut commands: Commands, asset_server: Res<AssetServer>) {
     let cup = asset_server.load(DICE_GLASS_CUP_SFX_PATH);
     let box_ = asset_server.load(DICE_WOODEN_BOX_SFX_PATH);
     commands.insert_resource(DiceCollisionSfx { cup, box_ });
+    commands.insert_resource(DiceCollisionSfxDebounce {
+        // "A little bit" of debounce: enough to stop contact spam, short enough to still feel responsive.
+        min_interval_s: 0.10,
+        ..Default::default()
+    });
 }
 
 pub fn play_dice_container_collision_sfx(
     mut commands: Commands,
     style: Res<DiceContainerStyle>,
     sfx: Res<DiceCollisionSfx>,
+    mut debounce: ResMut<DiceCollisionSfxDebounce>,
     mut collision_events: MessageReader<CollisionEvent>,
     dice_query: Query<(), With<Die>>,
     container_query: Query<(), Or<(With<DiceContainerVoxelCollider>, With<DiceContainerProceduralCollider>)>>,
     global_transforms: Query<&GlobalTransform>,
+    time: Res<Time>,
 ) {
+    let now_s = time.elapsed_secs();
+
     for ev in collision_events.read() {
         let CollisionEvent::Started(e1, e2, _flags) = *ev else {
             continue;
@@ -49,10 +69,18 @@ pub fn play_dice_container_collision_sfx(
         };
         let pos = die_gt.translation();
 
+        if let Some(last_s) = debounce.last_played_s.get(&die_entity) {
+            if now_s - *last_s < debounce.min_interval_s {
+                continue;
+            }
+        }
+
         let sound = match *style {
             DiceContainerStyle::Box => sfx.box_.clone(),
             DiceContainerStyle::Cup => sfx.cup.clone(),
         };
+
+        debounce.last_played_s.insert(die_entity, now_s);
 
         commands.spawn((
             AudioPlayer(sound),
